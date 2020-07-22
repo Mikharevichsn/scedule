d4f07b13d453b2ce2bfeb5cc414c96daed3a536f
Sergei Mikharevich <mikharevichsn@mail.ru>
2020-07-22 20:11:44 +0300

    date

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ app.set('view engine', 'hbs');
 app.get('/', (req, res) => {
   let date1 = new Date(2011, 0, 1, 0, 0, 0, 0); // // 1 января 2011, 00:00:00
   console.log(date1);
+  console.log(date1);
   let date2 = new Date(Date.parse('2020-07-03 10:00:00'));
   res.render('index', { date1, date2 });
 });
