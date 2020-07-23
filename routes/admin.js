d1bfcd07902d7699ec45d55df8ebf2cb626ea1f8
Eva <schennikova.ev@gmail.com>
2020-07-23 18:44:28 +0300

    service

--- routes/admin.js
+++ routes/admin.js
@@ -1,27 +1,33 @@
 const express = require('express');
 const Entry = require('../models/entry');
 const router = express.Router();
+const { Service } = require('../models/service');
 
 router.get('/', (req, res) => {
   res.render('admin');
 });
 
-router.get('/newEntry', (req, res) => {
-  res.render('adminNewEntry');
+router.get('/newEntry', async (req, res) => {
+  const services = await Service.find();
+  console.log(services);
+  res.render('adminNewEntry', { services });
+  // res.render('adminNewEntry');
 });
 
 router.post('/newEntry', async (req, res) => {
   console.log(req.body);
   const dateBegin = new Date(`${req.body.date}T${req.body.time}`);
   const dateEnd = new Date(dateBegin);
-  dateEnd.setHours(dateEnd.getHours() + 2);
+  const service = await Service.findById(req.body.serviceId)
+  dateEnd.setHours(dateEnd.getHours() + service.duration);
   // console.log(dateBegin);
-  const { nameClient, phone } = req.body;
+  const { nameClient, phone, serviceId } = req.body;
   await Entry.create({
     dateBegin,
     dateEnd,
     nameClient,
     phone,
+    serviceId
   });
   res.send(req.body);
 });
